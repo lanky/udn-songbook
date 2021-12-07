435a6534fe1acf4383e6ec1120d6cab2709090dc
Stuart Sears <stuart@sjsears.com>
2021-12-07 22:56:05 +0000

    renames classes, adds structure for layout

--- udn_songbook/renderer.py
+++ udn_songbook/renderer.py
@@ -2,16 +2,21 @@
 # wrapper around jinja2 so we can call it
 # from both song and book, and potentially
 # override or replace it
-
+import logging
 import os
-import sys
+
+# for function annotations
+from typing import Dict, List
 
 import jinja2
+from dynaconf import settings
+
+import udn_songbook
 
 from .filters import custom_filters
 
 
-class Renderer(object):
+class HTMLPublisher(object):
     """
     Generic class, will call out to jinja2 or any other rendering
     engine we choose to use
@@ -20,13 +25,31 @@ class Renderer(object):
     use that to output HTML documents
     """
 
-    def __init__(self, templatedirs=[], stylesheets=[]):
+    def __init__(
+        self,
+        songbook: udn_songbook.book.SongBook,
+        destdir: str,
+        templatedirs: List[str] = settings.TEMPLATEDIRS,
+        stylesheets: List[str] = settings.STYLESHEETS,
+    ):
         """
         creates  a jinja2 environment, loading templates from the specified
         directory, falling back on templates defined in this package
 
-        Same applies to css. Should probably move over to scss but there you go.
+        Same applies to css. Should probably move over to sass/scss but there you go.
+
+        Args:
+            songbook(udn_songbook.book.SongBook): songbook object to publish
+            destdir(str): top-level directory into which you want it published
+
+        KWargs:
+            templatedirs(list[str]): list of directories to search for templates
+            stylesheets(list[str]): list of stylesheets to include in output
         """
+        self.book = songbook
+        self.topdir = destdir
+        self.stylesheets = stylesheets
+
         self.env = jinja2.Environment(
             loader=jinja2.ChoiceLoader(
                 [
@@ -38,7 +61,37 @@ class Renderer(object):
         # update the filter list
         self.env.filters.update(custom_filters)
 
-    def render(self, template, context, **kwargs):
+    def build_structure(self, layout: List[str] = settings.LAYOUT):
+        """
+        Creates a directory structure into which we write our files
+        """
+        if not os.path.exists(self.topdir):
+            try:
+                os.makedirs(self.topdir)
+            except (IOError, OSError) as E:
+                self.book.__log(
+                    f"Unable to create output dir {E.filename} - {E.strerror}",
+                    logging.ERROR,
+                )
+                raise
+
+        for dname in sorted(layout):
+            try:
+                os.makedirs(os.path.join(self.topdir, dname))
+            except (IOError, OSError) as E:
+                self.book.__log(
+                    f"Unable to create output dir {E.filename} - {E.strerror}",
+                    logging.ERROR,
+                )
+                raise
+
+    def render(self, songbook: udn_songbook.book.SongBook, **kwargs):
+        """
+        top-level rendering method, which uses the two following methods
+        to render and link the index and
+        """
+
+    def render_index(self, context: Dict, template: str = "index.html.j2", **kwargs):
         """
         Render the chose templated  with the provided context
         The template will be searched for in the given paths, in order, stopping at
@@ -48,17 +101,11 @@ class Renderer(object):
         tpl = self.env.get_template(template)
         return tpl.render(context, **kwargs)
 
-
-class HTMLRenderer(Renderer):
-    """
-    The HTML Renderer uses Jinja2 templates to generate HTML pages and an index if appropriate
-    Each song will have HTML markup automatically generated via ukedown
-    """
-
-    pass
+    def render_song(self, context: Dict, template: str = "song.html.j2", **kwargs):
+        pass
 
 
-class PDFRenderer(Renderer):
+class PDFPublisher(HTMLPublisher):
     """
     The PDF Renderer processes one or more songsheets and returns them as rendered PDF docs
     """
