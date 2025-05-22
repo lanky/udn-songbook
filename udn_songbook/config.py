c20e1937ed29cd000f5755151f3e6a176f61aa60
Stuart Sears <stuart@sjsears.com>
2025-05-22 22:32:54 +0100

    add project settings file support

--- udn_songbook/config.py
+++ udn_songbook/config.py
@@ -13,20 +13,23 @@ DEFAULT_SETTINGS_FILES = [
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
 #
-def load_settings(settings_files: list[Path] = []) -> dynaconf.LazySettings:
+def load_settings(extra_settings: Path | None = None) -> dynaconf.LazySettings:
     """Load settings from the provided list of files.
 
     Args:
         settings_files(list[Path]): list of pathlib.Path objects to look for.
 
     """
-    # preprend custom config paths to settings_files
 
-    settings_files.extend(DEFAULT_SETTINGS_FILES)
+    filelist = (
+        DEFAULT_SETTINGS_FILES + [Path(extra_settings)]
+        if extra_settings is not None
+        else DEFAULT_SETTINGS_FILES
+    )
 
     return dynaconf.Dynaconf(
         envvar_prefix="UDN_SONGBOOK",
-        settings_files=settings_files,
+        settings_files=filelist,
         environments=False,
         merge_enabled=True,
         load_dotenv=True,
